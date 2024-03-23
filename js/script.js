1dce97cebc1246aa6486beebf406d76b690fe9aa
pphyo <pyaephyo.jdc@gmail.com>
2024-03-23 16:28:46 +0630

    Add favicon and seo

--- js/script.js
+++ js/script.js
@@ -9,47 +9,55 @@ const navItems = document.querySelectorAll(".nav a");
 const navToggler = document.querySelector(".nav-toggler");
 const aside = document.querySelector(".aside");
 
-for(let i = 0; i < navItems.length; i++) {
-   navItems[i].addEventListener("click", () => {
+(function() {
+   'use strict';
 
-      for(let j = 0; j < navItems.length; j++) {
-         if(navItems[j] == navItems[i]) {
-            navItems[j].classList.add("active");
-         } else {
-            navItems[j].classList.remove("active");
+   for(let i = 0; i < navItems.length; i++) {
+      navItems[i].addEventListener("click", () => {
+
+         for(let j = 0; j < navItems.length; j++) {
+            if(navItems[j] == navItems[i]) {
+               navItems[j].classList.add("active");
+            } else {
+               navItems[j].classList.remove("active");
+            }
          }
-      }
 
-      if(window.innerWidth <= 1199) {
-         aside.style.left = "-270px";
-         navToggler.style.left = "10px"
-      }
+         if(window.innerWidth <= 1199) {
+            aside.style.left = "-270px";
+            navToggler.style.left = "10px"
+         }
 
-   })
-}
+      })
+   }
+});
 
 
-if(window.innerWidth <= 1199) {
-   navToggler.addEventListener("click", () => {
-      
-      if(!aside.style.left) {
-         aside.style.left = "-270px";
-      }
+(function() {
+   'use strict';
 
-      if(aside.style.left == "-270px") {
-         aside.style.left = "0";
-         navToggler.style.left = "200px";
-      } else {
-         aside.style.left = "-270px";
-         navToggler.style.left = "10px";
-      }
-   })
-   window.addEventListener("resize", () => {
-      if(window.innerWidth > 1199) {
-         aside.style.left = null;
-      }
-   })
-}
+   if(window.innerWidth <= 1199) {
+      navToggler.addEventListener("click", () => {
+         
+         if(!aside.style.left) {
+            aside.style.left = "-270px";
+         }
+   
+         if(aside.style.left == "-270px") {
+            aside.style.left = "0";
+            navToggler.style.left = "200px";
+         } else {
+            aside.style.left = "-270px";
+            navToggler.style.left = "10px";
+         }
+      })
+      window.addEventListener("resize", () => {
+         if(window.innerWidth > 1199) {
+            aside.style.left = null;
+         }
+      })
+   }
+})();
 
 (function() {
    'use strict';
